08cd0b77a68f7877e6b66952cd64f0bf072b65e5
Masaaki Goshima <goccy54@gmail.com>
2016-06-15 15:06:19 +0900

    add load balance logic for default servers

--- ngx_http_upstream_available_capacity_module.c
+++ ngx_http_upstream_available_capacity_module.c
@@ -7,6 +7,22 @@
 #include <ngx_http.h>
 
 static char *ngx_http_upstream_available_capacity(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static void *ngx_http_upstream_available_capacity_create_conf(ngx_conf_t *cf);
+
+typedef struct {
+	ngx_http_upstream_server_t *server;
+	ngx_addr_t addr;
+	int capacity;
+} ngx_http_upstream_available_capacity_server_t;
+
+typedef struct {
+	ngx_http_upstream_srv_conf_t *conf;
+} ngx_http_upstream_available_capacity_data_t;
+
+typedef struct {
+	ngx_str_t host;
+	ngx_array_t *servers;
+} ngx_http_upstream_available_capacity_srv_conf_t;
 
 static ngx_command_t ngx_http_upstream_available_capacity_commands[] = {
     { 
@@ -21,17 +37,17 @@ static ngx_command_t ngx_http_upstream_available_capacity_commands[] = {
 };
 
 static ngx_http_module_t ngx_http_upstream_available_capacity_module_ctx = {
-    NULL,                              /* preconfiguration */
-    NULL,                              /* postconfiguration */
+    NULL,                                             /* preconfiguration */
+    NULL,                                             /* postconfiguration */
     
-    NULL,                              /* create main configuration */
-    NULL,                              /* init main configuration */
+    NULL,                                             /* create main configuration */
+    NULL,                                             /* init main configuration */
 
-    NULL,                              /* create server configuration */
-    NULL,                              /* merge server configuration */
+    ngx_http_upstream_available_capacity_create_conf, /* create server configuration */
+    NULL,                                             /* merge server configuration */
 
-    NULL,                              /* create location configuration */
-    NULL                               /* merge location configuration */
+    NULL,                                             /* create location configuration */
+    NULL                                              /* merge location configuration */
 };
 
 ngx_module_t ngx_http_upstream_available_capacity_module = {
@@ -51,31 +67,34 @@ ngx_module_t ngx_http_upstream_available_capacity_module = {
 
 static ngx_int_t ngx_http_upstream_get_available_capacity_peer(ngx_peer_connection_t *pc, void *data)
 {
+	ngx_http_upstream_available_capacity_data_t *cap_data = data;
     ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "get available capacity peer, try: %ui", pc->tries);
     pc->cached = 0;
     pc->connection = NULL;
 
-	ngx_url_t u;
-    ngx_memzero(&u, sizeof(ngx_url_t));
-	ngx_str_t url = ngx_string("127.0.0.1");
-    u.url = url;
-    u.default_port = 8002;
+	ngx_http_upstream_available_capacity_srv_conf_t *conf = ngx_http_conf_upstream_srv_conf(cap_data->conf, ngx_http_upstream_available_capacity_module);
+	ngx_http_upstream_available_capacity_server_t *found_server = NULL;
+	
+	size_t i = 0;
+	for (i = 0; i < conf->servers->nelts; ++i) {
+		ngx_http_upstream_available_capacity_server_t *servers = conf->servers->elts;
+		ngx_http_upstream_available_capacity_server_t *server  = &servers[i];
+		if (server->capacity > 0) {
+			found_server = server;
+			ngx_log_debug1(NGX_LOG_DEBUG_HTTP, pc->log, 0, "server_capacity = [%d]\n", server->capacity);
+			--server->capacity;
+			break;
+		}
+	}
 
-	ngx_pool_t *pool = ngx_create_pool(NGX_DEFAULT_POOL_SIZE, pc->log);
-
-    if (ngx_parse_url(pool, &u) != NGX_OK) {
-        if (u.err) {
-			fprintf(stderr, "error : %s\n", u.err);
-        }
-		ngx_destroy_pool(pool);
-        return NGX_ERROR;
-    }
-
-    pc->sockaddr = u.addrs->sockaddr;
-    pc->socklen = u.addrs->socklen;
-    pc->name = &u.url;
-
-	ngx_destroy_pool(pool);
+	if (found_server) {
+		pc->sockaddr = found_server->addr.sockaddr;
+		pc->socklen  = found_server->addr.socklen;
+		pc->name     = &found_server->server->name;
+	} else {
+		ngx_log_error_core(NGX_LOG_ERR, pc->log, 0, "cannot load balance");
+		return NGX_ERROR;
+	}
 	
     return NGX_OK;
 }
@@ -83,7 +102,10 @@ static ngx_int_t ngx_http_upstream_get_available_capacity_peer(ngx_peer_connecti
 static ngx_int_t ngx_http_upstream_init_available_capacity_peer(ngx_http_request_t *r, ngx_http_upstream_srv_conf_t *us)
 {
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "init available capacity peer");
-    r->upstream->peer.get = ngx_http_upstream_get_available_capacity_peer;
+	ngx_http_upstream_available_capacity_data_t *data = ngx_palloc(r->pool, sizeof(ngx_http_upstream_available_capacity_data_t));
+	data->conf = us;
+	r->upstream->peer.data = data;
+    r->upstream->peer.get  = ngx_http_upstream_get_available_capacity_peer;
     return NGX_OK;
 }
 
@@ -91,7 +113,38 @@ static ngx_int_t ngx_http_upstream_init_available_capacity(ngx_conf_t *cf, ngx_h
 {
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, cf->log, 0, "init available_capacity");
     us->peer.init = ngx_http_upstream_init_available_capacity_peer;
+	ngx_http_upstream_available_capacity_srv_conf_t *conf = ngx_http_conf_upstream_srv_conf(us, ngx_http_upstream_available_capacity_module);
+	conf->servers = ngx_array_create(cf->pool, us->servers->nelts, sizeof(ngx_http_upstream_available_capacity_server_t));
+
+	size_t i = 0;
+	for (i = 0; i < us->servers->nelts; ++i) {
+		ngx_http_upstream_server_t *servers = us->servers->elts;
+		ngx_http_upstream_available_capacity_server_t *cap_server = ngx_array_push(conf->servers);
+		cap_server->server   = &servers[i];
+		cap_server->capacity = 1;
+		
+		ngx_str_t server_address = ngx_string(cap_server->server->name.data);
+		ngx_addr_t addr;
+		ngx_memzero(&addr, sizeof(ngx_addr_t));
+
+		if (ngx_parse_addr_port(cf->pool, &addr, server_address.data, strlen((char *)server_address.data)) != NGX_OK) {
+			ngx_conf_log_error(NGX_LOG_ERR, cf, 0, "cannot parse url : %s", server_address.data);
+			return NGX_DECLINED;
+		}
+
+		cap_server->addr = addr;
+	}
     return NGX_OK;
+}
+
+static void *ngx_http_upstream_available_capacity_create_conf(ngx_conf_t *cf)
+{
+    ngx_http_upstream_available_capacity_srv_conf_t  *conf;
+    conf = ngx_palloc(cf->pool, sizeof(ngx_http_upstream_available_capacity_srv_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+    return conf;
 }
  
 static char *ngx_http_upstream_available_capacity(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
